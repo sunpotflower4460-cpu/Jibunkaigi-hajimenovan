b99064ea3738967cc05b7ccb91903ad93bf7ae6c
sunpotflower4460-cpu <sunpotflower4460@gmail.com>
2026-04-27 08:13:44 +0900

    feat: add Tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'Avenir', 'Helvetica', 'Arial', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
