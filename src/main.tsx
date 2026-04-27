4d53619465331ba578710f4d34c33d22115f3a08
sunpotflower4460-cpu <sunpotflower4460@gmail.com>
2026-04-27 10:52:02 +0900

    fix: install page auto-scroll helper

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppStable from './AppStable';
 import { ErrorBoundary } from './components/ErrorBoundary';
+import { installPageAutoScroll } from './utils/installPageAutoScroll';
 import './index.css';
+
+installPageAutoScroll();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
