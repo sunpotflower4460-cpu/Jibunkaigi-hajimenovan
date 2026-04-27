93e0a0d41abdf72ab9a3c5ba47e2bd6bf8ab8d25
sunpotflower4460-cpu <sunpotflower4460@gmail.com>
2026-04-27 11:08:39 +0900

    feat: hydrate localStorage from IndexedDB before render

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppStable from './AppStable';
 import { ErrorBoundary } from './components/ErrorBoundary';
+import { hydrateLocalStorageFromIndexedDb } from './services/storage';
 import { installPageAutoScroll } from './utils/installPageAutoScroll';
 import './index.css';
 
 installPageAutoScroll();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ErrorBoundary>
-      <AppStable />
-    </ErrorBoundary>
-  </React.StrictMode>,
-);
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+
+const renderApp = () => {
+  root.render(
+    <React.StrictMode>
+      <ErrorBoundary>
+        <AppStable />
+      </ErrorBoundary>
+    </React.StrictMode>,
+  );
+};
+
+void hydrateLocalStorageFromIndexedDb().finally(renderApp);
